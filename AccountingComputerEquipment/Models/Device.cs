594c35d27c1e9267af7b6d23d1060e872c435324
NikalsGG <126110457+NikalsGG@users.noreply.github.com>
2023-10-27 13:58:32 +0300

    [Feature] Add Device model

---
+++ AccountingComputerEquipment/Models/Device.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AccountingComputerEquipment.Models
+{
+    public abstract class Device
+    {
+        //public int DeviceId { get; set; }
+        [Key]
+        public int InventoryNumber { get; set; }
+        public string SerialNumber { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
