3424660e3422fe0bdc483cc42e60b584ca49bef2
NikalsGG <126110457+NikalsGG@users.noreply.github.com>
2023-10-27 14:01:10 +0300

    [Feature] Add Subdivision model

---
+++ AccountingComputerEquipment/Models/Subdivision.cs
@@ -0,0 +1,8 @@
+﻿namespace AccountingComputerEquipment.Models
+{
+    public class Subdivision
+    {
+        public int Id { get; set; }
+        public string SubdivisionName { get; set; }
+    }
+}
