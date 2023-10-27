c3d96089c4c22c294e5a42968c9e2dacbbe362aa
NikalsGG <126110457+NikalsGG@users.noreply.github.com>
2023-10-27 14:03:55 +0300

    [Feature] Add Computer model

---
+++ AccountingComputerEquipment/Models/Computer.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AccountingComputerEquipment.Models
+{
+    public class Computer : Device
+    {
+        public string SerialNumber { get; set; }
+        public DateTime BuildDate { get; set; }
+        public string CPU { get; set; }
+        public string GPU { get; set; }
+        public string RAM { get; set; }
+        public string Motherboard { get; set; }
+        public string HDD { get; set; }
+        public string LANCard { get; set; }
+        public string DVD { get; set; }
+        public string PSU { get; set; } // Блок питания
+    }
+}
