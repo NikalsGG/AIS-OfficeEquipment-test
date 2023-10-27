284d3548506a10a2f9b8d4bd6313d6dee44b065c
NikalsGG <126110457+NikalsGG@users.noreply.github.com>
2023-10-27 14:02:02 +0300

    [Feature] Add User model

---
+++ AccountingComputerEquipment/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Identity.Client;
+
+namespace AccountingComputerEquipment.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        // позиция в компании
+        public int? PositionId { get; set; }
+        public Position Position { get; set; }
+
+        // подразделение в компании
+        public int? SubdivisionId { get; set; }
+        public Subdivision Subdivision { get; set; }
+    }
+}
