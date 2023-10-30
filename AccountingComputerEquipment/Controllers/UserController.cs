867761f30e53277f947b5f1ce350d26333a47b66
NikalsGG <126110457+NikalsGG@users.noreply.github.com>
2023-10-30 21:10:53 +0300

    [Feature] Add User View

---
+++ AccountingComputerEquipment/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using AccountingComputerEquipment.Data;
+using AccountingComputerEquipment.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountingComputerEquipment.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<User> objUserList = _db.Users;
+            return View(objUserList);
+        }
+    }
+}
